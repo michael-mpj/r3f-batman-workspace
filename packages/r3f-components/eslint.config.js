35bb46e4a004d9f4abe7199dc49655ede4667f5a
Michael Joseph <michaeljoseph.mpj@gmail.com>
2026-05-29 07:42:36 +0400

    chore: update workspace settings and r3f lint config

--- packages/r3f-components/eslint.config.js
+++ packages/r3f-components/eslint.config.js
@@ -7,66 +7,66 @@
  * Version: 1.0.0
  */
 
-import js from '@eslint/js';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import js from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
 export default [
-    js.configs.recommended,
-    {
-        files: ['**/*.{js,jsx}'],
-        plugins: {
-            'react': reactPlugin,
-            'react-hooks': reactHooksPlugin,
-        },
-        languageOptions: {
-            ecmaVersion: 2023,
-            sourceType: 'module',
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-            globals: {
-                console: 'readonly',
-                process: 'readonly',
-                Buffer: 'readonly',
-                global: 'readonly',
-                __dirname: 'readonly',
-                __filename: 'readonly',
-                module: 'readonly',
-                require: 'readonly',
-                exports: 'readonly',
-            },
-        },
-        settings: {
-            react: {
-                version: '19.0',
-            },
-        },
-        rules: {
-            // Code quality
-            'no-unused-vars': [
-                'error',
-                {
-                    argsIgnorePattern: '^_',
-                    varsIgnorePattern: '^_',
-                },
-            ],
-            'no-console': 'warn',
-
-            // React rules
-            'react/jsx-uses-react': 'off',
-            'react/react-in-jsx-scope': 'off',
-            'react/prop-types': 'off',
-            'react/no-unknown-property': 'off',
-            'react-hooks/rules-of-hooks': 'error',
-            'react-hooks/exhaustive-deps': 'warn',
-
-            // Best practices (non-formatting)
-            'eqeqeq': ['error', 'always'],
-            'no-var': 'error',
-            'prefer-const': 'error',
-        },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    plugins: {
+      "react": reactPlugin,
+      "react-hooks": reactHooksPlugin,
     },
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        Buffer: "readonly",
+        global: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        module: "readonly",
+        require: "readonly",
+        exports: "readonly",
+      },
+    },
+    settings: {
+      react: {
+        version: "19.0",
+      },
+    },
+    rules: {
+      // Code quality
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "no-console": "warn",
+
+      // React rules
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/no-unknown-property": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+
+      // Best practices (non-formatting)
+      "eqeqeq": ["error", "always"],
+      "no-var": "error",
+      "prefer-const": "error",
+    },
+  },
 ];
